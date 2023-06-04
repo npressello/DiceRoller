b3df836811f87e5fd10efd2428fbb15b1e9fb8b7
npressello <npressello@gmail.com>
2023-06-04 13:03:01 -0300

    Added image to dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,7 +17,21 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         binding.rollButton.setOnClickListener{
-            binding.rolledTxt.text = "Rolled"
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = binding.diceImage
+        diceImage.setImageResource(drawableResource)
     }
 }
